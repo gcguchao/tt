6e4843cc66bdea7b44a6e6c04c9ea4edcb38da0d
guchao <guchao@xueqiu.com>
2021-04-30 17:46:53 +0800

    modify Test.java 20210430

--- src/com/guchao/tt/Test.java
+++ src/com/guchao/tt/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("world!");
-		
+		System.out.println("000");		
 		System.out.println("111");
 	}
 
